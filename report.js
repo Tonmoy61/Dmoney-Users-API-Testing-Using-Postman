17d96de52a611127c13b866046cfcdc43ba51a8e
MD REDWAN <tonmoy6060@gmail.com>
2022-10-09 16:37:29 +0600

    Add files via upload

---
+++ report.js
@@ -0,0 +1,14 @@
+var newman = require('newman');
+newman.run({
+collection: require('./Customer_API_Testing\Dmoney_api_collection.json'), //pointing to local JSON file.
+//environment: require('./collection/customer_api_env.json'), // pointing to local env file iterationCount: 1,
+reporters: 'htmlextra',
+reporter: {
+htmlextra: {
+export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+}
+}
+}, function (err) {
+if (err) { throw err; }
+console.log('collection run complete!');
+});
